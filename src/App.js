29ba31cbfc6d2708048940cf91267ee9acfac047
glauroqj <glauro.juliani@hotmail.com>
2018-07-01 20:46:02 -0300

    add: routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+/* pages */
+import Login from './pages/Login';
+import CreateAccount from './pages/CreateAccount';
+import Dashboard from './pages/Dashboard';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Switch>    
+          <Route exact path="/login" component={Login} />
+          <Route path="/create" component={CreateAccount} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Redirect to="/login" />
+      </Switch>
     );
   }
 }
