0e6caf817353f7a61401b70b76671b7ba212cc05
glauroqj <glauro.juliani@hotmail.com>
2018-07-02 21:43:36 -0300

    add: component create account

--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -1,15 +1,64 @@
 import React, { Component } from 'react';
+import '../assets/createAcc.css'
 
 class CreateAccount extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  handleInput(e) {
+    if(e.target.id === 'email') {
+      this.setState({
+        email: e.target.value
+      })
+    }
+
+    if(e.target.id === 'password') {
+      this.setState({
+        password: e.target.value
+      })
+    }
+  }
+  
+  createAcc() {
+    // window.location.href = '/create'
+  }
+
+  async submit() {
+
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">CREATE ACCOUNT</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="createAcc container">
+        <h5 className="createAcc_title">Criar Conta!</h5>
+        <form action="" className="createAcc_form">
+          <div className="form-group row justify-content-sm-center">
+            <label className="col-sm-2 col-form-label">Email</label>
+            <div className="col-sm-6">
+              <input type="text" className="form-control" id="email" placeholder="email@example.com" onChange={this.handleInput.bind(this)}/>
+            </div>
+          </div>
+          <div className="form-group row justify-content-sm-center">
+            <label className="col-sm-2 col-form-label">Senha</label>
+            <div className="col-sm-6">
+              <input type="password" className="form-control" id="password" placeholder="*****" onChange={this.handleInput.bind(this)}/>
+            </div>
+          </div>
+          <div className="form-group row justify-content-sm-center">
+            <label className="col-sm-2 col-form-label">Confirmar Senha</label>
+            <div className="col-sm-6">
+              <input type="password" className="form-control" id="confirm_password" placeholder="*****" onChange={this.handleInput.bind(this)}/>
+            </div>
+          </div>
+          <div className="form-group">
+            <button type="button" className="btn btn-success" onClick={this.createAcc}>Criar Conta</button>
+          </div>
+        </form>
       </div>
     );
   }
