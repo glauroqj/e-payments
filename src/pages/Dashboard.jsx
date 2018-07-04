40db678b9fafc004ba9413b63e0ce276f35b951d
glauroqj <glauro.juliani@hotmail.com>
2018-07-03 22:18:57 -0300

    add: dashboard page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,15 +1,53 @@
 import React, { Component } from 'react';
+// import { SemipolarSpinner } from 'react-epic-spinners'
+import { ToastContainer, toast } from 'react-toastify';
+import {verify} from '../components/modules/verifyLogin'
+import Navbar from '../components/Navbar';
+import Loader from '../components/Loader';
+
+import '../assets/dashboard.css'
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    }
+  }
+
+  componentWillMount() {
+    /* verify if user is logged */
+    verify().then((user) => {
+      console.log('LOGED: ', user)
+      /* redirect to dashboard */
+      this.setState({
+        user: user,
+        loading: false
+      });
+      return false;
+    })
+    .catch((error) => {
+      console.log('Not Loged: ')
+      this.props.history.push('/login');
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">DASHBOARD</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="dashboard">
+        {this.state.loading &&
+          <Loader text="Carregando Dashboard" color="#686de0"/>
+        }
+        {!this.state.loading &&
+          <div>
+            <ToastContainer autoClose={5000} hideProgressBar={true} position="top-right"/>
+            <Navbar />
+            <div className="container">
+
+            </div>
+          </div>
+        }
+        {/* end loading */}
       </div>
     );
   }
