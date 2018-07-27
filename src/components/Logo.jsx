9d452149380ecb9443af17faeb5a849b9fb62a48
glauroqj <glauro.juliani@hotmail.com>
2018-07-27 00:41:00 -0300

    fix: logo name

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,8 +5,8 @@ class Logo extends Component {
   render() {
     return (
       <div className="logo">
-        <img className="img-responsive" src={logoIMG} alt="Logo Equalizar" title="Logo Equalizar"/>
-        <h2>Equalizar Doações</h2>
+        <img className="img-responsive" src={logoIMG} alt="Logo Equale" title="Logo Equale"/>
+        <h2>Equale Doações</h2>
       </div>
     );
   }
