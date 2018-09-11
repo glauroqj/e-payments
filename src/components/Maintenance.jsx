e6d9df943d29e100761a454aa76df800fdf19dfe
glauroqj <glauro.juliani@hotmail.com>
2018-09-10 22:36:19 -0300

    deploy

--- src/components/Maintenance.jsx
+++ src/components/Maintenance.jsx
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import InputRange from 'react-input-range'
+import 'react-input-range/lib/css/index.css'
+
+import student from '../assets/images/student.png'
+import ong from '../assets/images/ong.png'
 
 class Maintenance extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      link: 'https://pagseguro.uol.com.br/checkout/nc/nl/donation/sender-identification.jhtml'
+      link: 'https://pagseguro.uol.com.br/checkout/nc/nl/donation/sender-identification.jhtml',
+      value: 5
     }
   }
 
@@ -17,27 +23,120 @@ class Maintenance extends Component {
     return (
       <div className="container maintenance text-center animated fadeIn">
         <div className="jumbotron">
-          <h1 className="display-3">Olá!</h1>
-          <h4 className="">Esta é a nova plataforma para doações do Equale.</h4>
-          <h4 className="mb-3">Aqui você poderá fazer suas doações diretamente, de forma segura, simples e rápida.</h4>
+          <h1 className="display-5">Bem-Vinda(o)!!!</h1>
+          <h5 className="mb-2">O Instituto Equale trabalha diariamente para desenvolver a educação pública e popular em Minas Gerais.</h5>
+          <h5 className="mb-2">As doações que recebemos são utilizadas para desenvolver nossos projetos <a target="new" href="http://equale.com.br/projetos-equale">Projeto Equale</a> e para a sustentação da equipe executiva do Instituto.</h5>
+          <h5 className="mb-2">Aqui você poderá fazer suas doações diretamente, de forma segura, simples e rápida.</h5>
+         
           <hr className="my-4" />
-          <p>No momento estamos trabalhando muito na plataforma, mas você já pode fazer sua doação agora mesmo no site do nosso parceiro PagSeguro.</p>
-          <p>Basta clicar no botão logo abaixo e seguir estes passos</p>
 
-          <div className="card border-success mb-3">
-            <div className="card-header">Passo a Passo</div>
+          <blockquote className="blockquote text-center">
+            <p className="mb-1">Com <b className="text-success">R$120,00</b> por ano, ou <b className="text-success">R$120,00</b> por mês, somos capazes de garantir o material didático de um  estudante de cursinho popular</p>
+          </blockquote>
+
+          <blockquote className="blockquote text-center">
+            <p className="mb-1">Com <b className="text-success">R$162,00</b> por mês, arcamos com as passagens de um estudante de cursinho popular residente em Belo Horizonte.</p>
+            <p className="mb-1">Dez pessoas contribuindo com <b className="text-success">R$162,00</b> por mês, conseguem sustentar esse mesmo estudante.</p>
+          </blockquote>
+
+          <blockquote className="blockquote text-center">
+            <p className="mb-0">A manutenção da nossa Equipe Executiva, considerando taxas, impostos, material de escritório e a manutenção de dois funcionários custa, mensalmente, <b className="text-success">R$8.000,00</b>.</p>
+          </blockquote>
+
+          <div className="card border-secondary mb-3">
             <div className="card-body">
-              <p className="card-text">Após clicar no botão, você será redirecionado para o site do PagSeguro</p>
-              <p className="card-text">Escolha o valor que deseja doar, pode escolher qualquer valor</p>
-              <p className="card-text">Informe seu e-mail e selecione entre: "Comprar com minha conta pagSeguro" ou "Não tenho conta no PagSeguro"</p>
-              <p className="card-text">Na próxima tela basta escolher entre cartão de crédito ou boleto</p>
-              <p className="card-text">Agora é só confirmar o pagamento</p>
-            </div>
-          </div>
+              <h4>Deslize a barra abaixo para conhecer nossos planos de doação recorrente!</h4>
+              <InputRange
+                maxValue={5}
+                minValue={1}
+                value={this.state.value}
+                formatLabel={value => `${value}`}
+                onChange={value => this.setState({ value })} 
+              />
+              <div className="row mt-5">
+                <div className="col-sm-6">
+                  {/* aluno */}
+                  <div className="card amount-box mb-3">
+                    <h3 className="card-header">
+                      Adote um aluno
+                      {this.state.value === 5 &&
+                        <div className="animated pulse text-success amount-donation"> 100%</div>
+                      }
+                      {this.state.value === 4 &&
+                        <div className="animated pulse text-success amount-donation"> 50%</div>
+                      }
+                      {this.state.value === 3 &&
+                        <div className="animated pulse text-success amount-donation"> 20%</div>
+                      }
+                      {this.state.value === 2 &&
+                        <div className="animated pulse text-success amount-donation"> 10%</div>
+                      }
+                    </h3>
+                    <img className="amount-img" src={student} alt="Estudande" title="Estudante" />
+                    <div className="card-body">
+                      {this.state.value === 5 &&
+                        <a href="http://pag.ae/bkDXSKx" className="btn btn-success btn-lg" >quero doar R$162,00</a>
+                      }
+                      {this.state.value === 4 &&
+                        <a href="http://pag.ae/bhDXSyP" className="btn btn-success btn-lg" >quero doar R$81,00</a>
+                      }
+                      {this.state.value === 3 &&
+                        <a href="http://pag.ae/bkDXSpX" className="btn btn-success btn-lg" >quero doar R$32,40</a>
+                      }
+                      {this.state.value === 2 &&
+                        <a href="http://pag.ae/blDXSdQ" className="btn btn-success btn-lg" >quero doar R$16,20</a>
+                      }
+                    </div>
+                  </div>
+                </div> {/* col-xs-6 */}
 
-          <p className="lead">
-            <button type="button" className="btn btn-success btn-lg" onClick={this.doantion}>quero doar agora</button>
-          </p>
+                <div className="col-sm-6">
+                  {/* ong */}
+                  <div className="card amount-box mb-3">
+                    <h3 className="card-header">
+                      Amigo do Equale
+                      {this.state.value === 5 &&
+                        <div className="animated pulse text-success amount-donation"> Jaqueline Moll</div>
+                      }
+                      {this.state.value === 4 &&
+                        <div className="animated pulse text-success amount-donation"> Paulo Freire</div>
+                      }
+                      {this.state.value === 3 &&
+                        <div className="animated pulse text-success amount-donation"> Darcy Ribeiro</div>
+                      }
+                      {this.state.value === 2 &&
+                        <div className="animated pulse text-success amount-donation"> Anísio Teixeira</div>
+                      }
+                    </h3>
+                    <img className="amount-img" src={ong} alt="Equale" title="Equale"/>
+                    <div className="card-body">
+                      {this.state.value === 5 &&
+                        <a href="http://pag.ae/bjDXRw4" className="btn btn-success btn-lg" >quero doar R$500,00</a>
+                      }
+                      {this.state.value === 4 &&
+                        <a href="http://pag.ae/7U7ZUKRsG" className="btn btn-success btn-lg" >quero doar R$100,00</a>
+                      }
+                      {this.state.value === 3 &&
+                        <a href="http://pag.ae/7U7-4WuhG" className="btn btn-success btn-lg" >quero doar R$40,00</a>
+                      }
+                      {this.state.value === 2 &&
+                        <a href="http://pag.ae/blDXQzY" className="btn btn-success btn-lg" >quero doar R$20,00</a>
+                      }
+                    </div>
+                  </div>
+
+                </div> {/* col-xs-6 */}
+              </div>
+              
+              {this.state.value === 1 &&              
+                <p className="lead animated fadeIn">
+                  <button type="button" className="btn btn-success btn-lg btn-block" onClick={this.doantion}>quero doar qualquer valor</button>
+                </p>
+              }
+
+            </div> {/* card-body */}
+
+          </div>
 
           <form id="donate-pagseguro" className="hide" action="https://pagseguro.uol.com.br/checkout/v2/donation.html" method="post">
             <input name="currency" type="hidden" value="BRL"/>
