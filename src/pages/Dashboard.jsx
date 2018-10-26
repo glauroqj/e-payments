5eb9b2763ae96f959a80bea82cf6357d4a753b15
glauroqj <glauro.juliani@hotmail.com>
2018-10-25 23:28:54 -0300

    deploy

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -26,7 +26,7 @@ class Dashboard extends Component {
       idSession: '',
       radio: 'option1',
       form: {},
-      maintenance: false /* change to true */
+      maintenance: true /* change to true */
     }
   }
 
